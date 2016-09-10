690eb89f1d7f8662227966ff3aa3edfac128d254
wiztigers <regis.witz@free.fr>
2016-09-10 12:18:50 +0200

    addi V$ test project

---
+++ cshapp/test/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace cshapp.test {
+
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class ProgramTest {
+	[TestMethod]
+	public void TestMethod1() {
+		Assert.AreEqual(2, Program.f(1,1));
+	}
+}
+
+}
