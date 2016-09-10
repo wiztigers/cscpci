c686b0e72bc7abd27f5d617cf3d9331e3f833e2f
wiztigers <regis.witz@free.fr>
2016-09-10 11:50:24 +0200

    initial commit, with a lot less clutter in config files than usual

---
+++ cshapp/src/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace cshapp {
+
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+
+public class Program {
+
+	public static int f(int x, int y) {
+		return x+y;
+	}
+
+	static void Main(string[] args) {
+		System.Console.WriteLine("Hello, world");
+		foreach(var arg in args) System.Console.WriteLine("arg: "+arg);
+	}
+}
+
+}
